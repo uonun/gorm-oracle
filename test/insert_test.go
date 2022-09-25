5118ff402440e2325b5ea1ad2d9526031a67e46f
uonun <uonun@udnz.com>
2022-09-25 23:11:37 +0800

    feat: returning "autoIncrement" columns(single insert)

--- test/insert_test.go
+++ test/insert_test.go
@@ -2,9 +2,12 @@ package test
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/google/uuid"
+	"gorm.io/gorm/clause"
 )
 
 func TestInsertRaw(t *testing.T) {
@@ -34,6 +37,19 @@ func TestInsertModel(t *testing.T) {
 	checkTxError(t, db.Create(&c))
 }
 
+func TestInsertModelReturning(t *testing.T) {
+	c := getRandomCustomerReturning("TestInsertModelReturning")
+
+	db := getDb(t)
+	checkTxError(t, db.Create(&c).Clauses(clause.Returning{}))
+
+	if c.CustomerID <= 0 {
+		t.Errorf("create failed or not returning new id")
+	}
+
+	t.Logf("created: %d", c.CustomerID)
+}
+
 func TestInsertModels(t *testing.T) {
 	count := 4
 	batchId := uuid.NewString()
@@ -43,8 +59,33 @@ func TestInsertModels(t *testing.T) {
 	}
 
 	db := getDb(t)
+	tx := checkTxError(t, db.Create(&cs).Clauses(clause.Returning{}))
+	if tx.RowsAffected != int64(count) {
+		t.Errorf("batch insert affected %d rows, %d expected", tx.RowsAffected, count)
+	}
+}
+
+func TestInsertModelsReturning(t *testing.T) {
+	count := 4
+	batchId := uuid.NewString()
+	cs := make([]CustomerReturning, count)
+	for i := 0; i < count; i++ {
+		cs[i] = getRandomCustomerReturning(fmt.Sprintf("TestInsertModels:batch-%s:", batchId))
+	}
+
+	db := getDb(t)
 	tx := checkTxError(t, db.Create(&cs))
 	if tx.RowsAffected != int64(count) {
 		t.Errorf("batch insert affected %d rows, %d expected", tx.RowsAffected, count)
 	}
+
+	ids := make([]string, count)
+	for i := 0; i < count; i++ {
+		cid := cs[i].CustomerID
+		ids[i] = strconv.FormatInt(cid, 10)
+		if cid == 0 {
+			t.Errorf("not returning created value")
+		}
+	}
+	t.Logf("created: %s", strings.Join(ids, ","))
 }
