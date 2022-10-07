318513434f8a54afa8c171944f7b27bf7dd70fdf
Austin Luo <uonun@users.noreply.github.com>
2022-10-08 01:24:45 +0800

    Feature/bulk insert with returning columns (#1)
    * feat: bulk insert with returning sequence column.
    
    * feat: bulk insert with returning columns
    
    * fix: allow multiple returning columns now.
    
    * test: UPDATE clauses
    
    Co-authored-by: austinluo <austinluo@tencent.com>

--- test/models.go
+++ test/models.go
@@ -1,17 +1,23 @@
 package test
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // Customer table comment
+// use `sequence` to creating sequence columns.
 type Customer struct {
-	CustomerName string    `gorm:"column:CUSTOMER_NAME" json:"customer_name"`
-	CustomerID   int64     `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S" json:"customer_id"`
-	Address      string    `gorm:"column:ADDRESS" json:"address"`
-	City         string    `gorm:"column:CITY" json:"city"`
-	State        string    `gorm:"column:STATE" json:"state"`
-	ZipCode      string    `gorm:"column:ZIP_CODE" json:"zip_code"`
-	CreatedTime  time.Time `gorm:"column:CREATED_TIME" json:"created_time"`
-	Age          int32     `gorm:"column:AGE" json:"age"`
+	CustomerName string `gorm:"column:CUSTOMER_NAME" json:"customer_name"`
+	// test case: the sequence column is not the first
+	CustomerID  int64     `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S" json:"customer_id"`
+	Address     string    `gorm:"column:ADDRESS" json:"address"`
+	City        string    `gorm:"column:CITY" json:"city"`
+	State       string    `gorm:"column:STATE" json:"state"`
+	ZipCode     string    `gorm:"column:ZIP_CODE" json:"zip_code"`
+	CreatedTime time.Time `gorm:"column:CREATED_TIME" json:"created_time"`
+	Age         int32     `gorm:"column:AGE" json:"age"`
 }
 
 // TableName sets the insert table name for this struct type
@@ -19,19 +25,64 @@ func (c *Customer) TableName() string {
 	return "Customers"
 }
 
-// Customer table comment
+// CustomerReturning table comment
+// use `autoIncrement` to returning sequence columns.
 type CustomerReturning struct {
 	CustomerName string    `gorm:"column:CUSTOMER_NAME" json:"customer_name"`
-	CustomerID   int64     `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S;autoIncrement" json:"customer_id"`
 	Address      string    `gorm:"column:ADDRESS" json:"address"`
 	City         string    `gorm:"column:CITY" json:"city"`
 	State        string    `gorm:"column:STATE" json:"state"`
 	ZipCode      string    `gorm:"column:ZIP_CODE" json:"zip_code"`
 	CreatedTime  time.Time `gorm:"column:CREATED_TIME" json:"created_time"`
 	Age          int32     `gorm:"column:AGE" json:"age"`
+	// test case: the returning column `autoIncrement` is at the end
+	CustomerID int64 `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S;autoIncrement" json:"customer_id"`
 }
 
 // TableName sets the insert table name for this struct type
 func (c *CustomerReturning) TableName() string {
 	return "Customers"
+}
+
+// CustomerReturningPrimaryKey table comment
+// use `primaryKey` to create WHERE condition.
+type CustomerReturningPrimaryKey struct {
+	CustomerName string    `gorm:"column:CUSTOMER_NAME" json:"customer_name"`
+	Address      string    `gorm:"column:ADDRESS" json:"address"`
+	City         string    `gorm:"column:CITY" json:"city"`
+	State        string    `gorm:"column:STATE" json:"state"`
+	ZipCode      string    `gorm:"column:ZIP_CODE" json:"zip_code"`
+	CreatedTime  time.Time `gorm:"column:CREATED_TIME" json:"created_time"`
+	Age          int32     `gorm:"column:AGE" json:"age"`
+	// test case: the returning column `autoIncrement` is at the end
+	CustomerID int64 `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S;autoIncrement;primaryKey" json:"customer_id"`
+}
+
+// TableName sets the insert table name for this struct type
+func (c *CustomerReturningPrimaryKey) TableName() string {
+	return "Customers"
+}
+
+// CustomerHook table comment
+// use `sequence` to creating sequence columns.
+type CustomerHook struct {
+	CustomerName string `gorm:"column:CUSTOMER_NAME" json:"customer_name"`
+	// test case: the sequence column is not the first
+	CustomerID  int64     `gorm:"column:CUSTOMER_ID;sequence:CUSTOMERS_S" json:"customer_id"`
+	Address     string    `gorm:"column:ADDRESS" json:"address"`
+	City        string    `gorm:"column:CITY" json:"city"`
+	State       string    `gorm:"column:STATE" json:"state"`
+	ZipCode     string    `gorm:"column:ZIP_CODE" json:"zip_code"`
+	CreatedTime time.Time `gorm:"column:CREATED_TIME" json:"created_time"`
+	Age         int32     `gorm:"column:AGE" json:"age"`
+}
+
+// TableName sets the insert table name for this struct type
+func (c *CustomerHook) TableName() string {
+	return "Customers"
+}
+
+func (c *CustomerHook) BeforeCreate(tx *gorm.DB) (err error) {
+	c.State = "HOOK:" + c.State
+	return nil
 }
