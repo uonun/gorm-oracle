a643a4d6cd8310a4a266e680673d01bc21dbcfdc
austinluo <austinluo@tencent.com>
2022-10-09 10:46:34 +0800

    improvement: QuoteTo

--- oracle.go
+++ oracle.go
@@ -588,51 +588,7 @@ func (dialector Dialector) BindVarParameter(stmt *gorm.Statement) string {
 }
 
 func (dialector Dialector) QuoteTo(writer clause.Writer, str string) {
-	var (
-		underQuoted, selfQuoted bool
-		continuousBacktick      int8
-		shiftDelimiter          int8
-	)
-
-	for _, v := range []byte(str) {
-		switch v {
-		case '`':
-			continuousBacktick++
-			if continuousBacktick == 2 {
-				writer.WriteString("``")
-				continuousBacktick = 0
-			}
-		case '.':
-			if continuousBacktick > 0 || !selfQuoted {
-				shiftDelimiter = 0
-				underQuoted = false
-				continuousBacktick = 0
-				writer.WriteByte('`')
-			}
-			writer.WriteByte(v)
-			continue
-		default:
-			if shiftDelimiter-continuousBacktick <= 0 && !underQuoted {
-				// writer.WriteByte('`')
-				underQuoted = true
-				if selfQuoted = continuousBacktick > 0; selfQuoted {
-					continuousBacktick -= 1
-				}
-			}
-
-			for ; continuousBacktick > 0; continuousBacktick -= 1 {
-				// writer.WriteString("``")
-			}
-
-			writer.WriteByte(v)
-		}
-		shiftDelimiter++
-	}
-
-	if continuousBacktick > 0 && !selfQuoted {
-		writer.WriteString("``")
-	}
-	// writer.WriteByte('`')
+	writer.WriteString(str)
 }
 
 func (dialector Dialector) Explain(sql string, vars ...interface{}) string {
